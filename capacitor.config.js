d61d10d37eece1de21fe4caba59981b7a24b152e
Arsik123 <50909926+Arsik123@users.noreply.github.com>
2025-06-03 14:09:03 +0500

    Update and rename capacitor.config.ts to capacitor.config.js

---
+++ capacitor.config.js
@@ -0,0 +1,6 @@
+{
+  "appId": "com.example.mindar",
+  "appName": "MindAR Demo",
+  "webDir": "www",
+  "bundledWebRuntime": false
+}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId:    'com.example.mindar',  // любое ID
-  appName:  'MindAR Demo',
-  webDir:   'www',                 // папка c index.html
-  bundledWebRuntime: false
-};
-export default config;
